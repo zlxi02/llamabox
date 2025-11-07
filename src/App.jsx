76878fa156003e0d7ab63571e93e9f89a7be33e1
Zachary Xi <zachary.xi@gmail.com>
2025-11-06 23:25:03 -0800

    add reset button

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import Ground from './components/Ground'
 import Box from './components/Box'
+import ResetButton from './components/ResetButton'
 
 function App() {
+  const handleReset = () => {
+    // Will implement this in Phase 3
+    console.log('Reset clicked!')
+  }
+
   return (
     <div className="app">
       <h1>🦙 LlamaBox</h1>
       <p>Click anywhere to spawn a llama!</p>
       
       <Box />
+      <ResetButton onClick={handleReset} />
       <Ground />
     </div>
   )
