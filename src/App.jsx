7b24d1bec10b777e244e3cd55390ae58c9d504ce
Zachary Xi <zachary.xi@gmail.com>
2025-11-06 23:11:55 -0800

    Initial commit - vite framework for llamabox

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🦙 LlamaBox</h1>
+      <p>Click anywhere to spawn a llama!</p>
+    </div>
+  )
+}
+
+export default App
